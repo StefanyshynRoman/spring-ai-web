e2fc7fcfd39123c1b52cb3bc2fd5841d4edba2a1
roman <romanpz051@gmail.com>
2024-08-29 08:18:05 +0200

    0.0.0.1 -init

---
+++ src/main/java/com/stefanyshyn/springaiweb/SpringAiWebApplication.java
@@ -0,0 +1,13 @@
+package com.stefanyshyn.springaiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefanyshyn/springaiweb/SpringAiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanyshyn.springaiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
